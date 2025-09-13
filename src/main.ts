a48f64e757d5d266a98081abf14efe231e2c698e
TraumDE <eugene301workmaik@gmail.com>
2025-09-13 14:14:48 +0300

    Add vue-tg package

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { VueTelegramPlugin } from 'vue-tg'
 
 const app = createApp(App)
 
+app.use(VueTelegramPlugin)
 app.use(router)
 
 app.mount('#app')
