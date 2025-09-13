491d5c6b3f408ec0df41b41ef9840e347de74921
TraumDE <eugene301workmaik@gmail.com>
2025-09-13 14:27:09 +0300

    test feature

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 import { Alert } from 'vue-tg'
-import { FullscreenViewport } from 'vue-tg'
+import { ExpandedViewport } from 'vue-tg'
 </script>
 
 <template>
-  <FullscreenViewport />
+  <expanded-viewport />
   <h1>You did it!</h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
