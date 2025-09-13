b278cbe9c375212ec2398b3548e5103a0143a0df
TraumDE <eugene301workmaik@gmail.com>
2025-09-13 20:45:36 +0300

    test buttons feature

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
 import { Alert } from 'vue-tg'
 import { FullscreenViewport } from 'vue-tg'
 import { ExpandedViewport } from 'vue-tg'
+import { MainButton } from 'vue-tg'
+import { SecondaryButton } from 'vue-tg'
 </script>
 
 <template>
@@ -12,6 +14,8 @@ import { ExpandedViewport } from 'vue-tg'
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  <main-button text="Main button" />
+  <secondary-button text="Secondary button" />
   <alert message="Hello!" />
 </template>
 
